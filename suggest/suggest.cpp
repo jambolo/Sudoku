0f5f9eedb4671f7e3b6f8477fa9cae5c41c63ee4
John Bolton <odolvlobo-github@yahoo.com>
2017-11-24 15:39:01 -0800

    Added another level of output. Changed to a verbosity level enum.

--- suggest/suggest.cpp
+++ suggest/suggest.cpp
@@ -2,18 +2,26 @@
 #include "Board/Board.h"
 
 #include <cstdio>
-#include <ctime>
+
+enum Verbosity
+{
+    QUIET,
+    VERBOSE,
+    DETAILED,
+    CHATTY
+};
 
 static void syntax()
 {
     fprintf(stderr, "syntax: suggest [-v][-a] <81 values, 0-9>\n");
     fprintf(stderr, "Options:\n");
-    fprintf(stderr, "  -v:  outputs additional information\n");
-    fprintf(stderr, "  -vv: outputs a detailed explanation for each step\n");
-    fprintf(stderr, "  -a:  outputs the complete solution.\n");
+    fprintf(stderr, "  -v:   outputs additional information\n");
+    fprintf(stderr, "  -vv:  outputs additional additional information\n");
+    fprintf(stderr, "  -vvv: outputs additional additional additional information\n");
+    fprintf(stderr, "  -a:   outputs the complete solution.\n");
 }
 
-static void printStep(Analyzer::Step const & step, bool verbose, bool detailed, int i = 0)
+static void printStep(Analyzer::Step const & step, Verbosity verbosity, int i = 0)
 {
     if (i > 0)
         printf("%3d. ", i);
@@ -23,10 +31,10 @@ static void printStep(Analyzer::Step const & step, bool verbose, bool detailed, 
         case Analyzer::Step::SOLVE:
         {
             printf("The value of %s is %d", Board::locationName(step.indexes[0]).c_str(), step.values[0]);
-            if (verbose || detailed)
+            if (verbosity >= VERBOSE || verbosity >= DETAILED)
             {
                 printf(" (%s)", Analyzer::Step::techniqueName(step.technique));
-                if (detailed)
+                if (verbosity >= DETAILED)
                     printf("\n     %s", step.reason.c_str());
             }
             printf("\n");
@@ -46,10 +54,10 @@ static void printStep(Analyzer::Step const & step, bool verbose, bool detailed, 
                 Board::locationOf(v, &r, &c);
                 printf("%s ", Board::locationName(v).c_str());
             }
-            if (verbose || detailed)
+            if (verbosity >= VERBOSE || verbosity >= DETAILED)
             {
                 printf(" (%s)", Analyzer::Step::techniqueName(step.technique));
-                if (detailed)
+                if (verbosity >= DETAILED)
                     printf("\n     %s", step.reason.c_str());
             }
             printf("\n");
@@ -66,9 +74,8 @@ static void printStep(Analyzer::Step const & step, bool verbose, bool detailed, 
 
 int main(int argc, char ** argv)
 {
-    bool verbose  = false;
-    bool all      = false;
-    bool detailed = false;
+    bool all            = false;
+    Verbosity verbosity = QUIET;
 
     --argc;
     ++argv;
@@ -81,20 +88,25 @@ int main(int argc, char ** argv)
     if (strcmp(*argv, "-a") == 0)
     {
         all     = true;
-        verbose = true;
+        verbosity = VERBOSE;
         ++argv;
         --argc;
     }
     if (strcmp(*argv, "-v") == 0)
     {
-        verbose = true;
+        verbosity = VERBOSE;
         ++argv;
         --argc;
     }
     if (strcmp(*argv, "-vv") == 0)
     {
-        verbose  = true;
-        detailed = true;
+        verbosity = DETAILED;
+        ++argv;
+        --argc;
+    }
+    if (strcmp(*argv, "-vvv") == 0)
+    {
+        verbosity = CHATTY;
         ++argv;
         --argc;
     }
@@ -129,12 +141,15 @@ int main(int argc, char ** argv)
         return 3;
     }
 
-    Analyzer analyzer(board, verbose);
+    Analyzer analyzer(board, verbosity >= VERBOSE);
 
-    if (verbose)
+    if (verbosity >= VERBOSE)
     {
         printf("Board to solve:\n");
-        analyzer.board().draw();
+        if (verbosity >= DETAILED)
+            analyzer.drawPenciledBoard();
+        else
+            analyzer.board().draw();
         printf("\n");
     }
 
@@ -144,11 +159,13 @@ int main(int argc, char ** argv)
         do
         {
             Analyzer::Step step = analyzer.next();
-            printStep(step, verbose, detailed, i++);
+            printStep(step, verbosity, i++);
+            if (verbosity >= CHATTY && (step.action == Analyzer::Step::SOLVE || step.action == Analyzer::Step::ELIMINATE))
+                analyzer.drawPenciledBoard();
         } while (!analyzer.done());
 
         printf("\n");
-        if (analyzer.stuck() && detailed)
+        if (analyzer.stuck() && verbosity >= DETAILED)
             analyzer.drawPenciledBoard();
         else
             analyzer.board().draw();
@@ -161,13 +178,15 @@ int main(int argc, char ** argv)
         do
         {
             step = analyzer.next();
-            printStep(step, verbose, detailed, i++);
-        } while (step.action != Analyzer::Step::SOLVE && !analyzer.done());
+            printStep(step, verbosity, i++);
+            if (verbosity >= CHATTY && step.action == Analyzer::Step::ELIMINATE)
+                analyzer.drawPenciledBoard();
+        } while (step.action == Analyzer::Step::ELIMINATE);
 
         printf("\n");
-        if (detailed)
+        if (verbosity >= DETAILED)
             analyzer.drawPenciledBoard();
-        else if (verbose)
+        else if (verbosity >= VERBOSE)
             analyzer.board().draw();
     }
 
